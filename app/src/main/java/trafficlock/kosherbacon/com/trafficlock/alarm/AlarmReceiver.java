6f95758c70e5d75d89ff05fa8d370dbcb7331b2c
Joshua Kahn <j.harris.kahn@gmail.com>
2016-08-15 22:24:09 -1000

    Added AlarmReceiver class.

---
+++ app/src/main/java/trafficlock/kosherbacon/com/trafficlock/alarm/AlarmReceiver.java
@@ -0,0 +1,15 @@
+package trafficlock.kosherbacon.com.trafficlock.alarm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by jkahn on 8/15/16.
+ */
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
