6a2f4dbda30c77076c6ad76c8d8b4c9a4b34b935
Joshua Kahn <j.harris.kahn@gmail.com>
2016-08-15 14:43:01 -1000

    Cleanup repository. Added basic layout.

--- app/src/main/java/trafficlock/kosherbacon/com/trafficlock/MainActivity.java
+++ app/src/main/java/trafficlock/kosherbacon/com/trafficlock/MainActivity.java
@@ -2,6 +2,9 @@ package trafficlock.kosherbacon.com.trafficlock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation welcomeAnim = new AlphaAnimation(0.2f, 1.0f);
+        welcomeAnim.setDuration(4 * 1000);
+        TextView welcomeMsg = (TextView) findViewById(R.id.welcome_msg);
+        welcomeMsg.startAnimation(welcomeAnim);
     }
 }
